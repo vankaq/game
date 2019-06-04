02ac3e983beeebb1b863c89320a4c4ef3f67b453
vankaq <33707034+vankaq@users.noreply.github.com>
2019-06-04 17:10:28 +0900

    Add files via upload

---
+++ Scripts/enemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class enemyController : MonoBehaviour
+{	
+	public Transform player;
+	public NavMeshAgent agent;
+
+     public Text scoreText;
+     static int score;
+    // Start is called before the first frame update
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        agent.SetDestination(player.transform.position);
+       
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.tag == "bullet"){   
+            Destroy(gameObject);
+            score = score + 1; 
+            scoreText.text = "score: " +score;
+
+        if(score == 4){
+               SceneManager.LoadScene(0);
+                score = 0;
+            }
+        }
+    }   
+}
---
+++ Scripts/playerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class playerController : MonoBehaviour
+{	
+	Rigidbody rb;
+    public Text hpText;
+   
+    int hp;
+    
+    public GameObject bullet;
+    GameObject bulletClone;
+    Rigidbody rbClone;
+    // Start is called before the first frame update
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        hp = 100;
+        //x = transform.position.x;
+        //y = transform.position.y;
+        //z = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.AddForce(transform.forward * moveVertical * 50f);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb.AddForce(transform.right * moveHorizontal * 20f);
+
+        if (Input.GetKey("space"))
+       {
+            bulletClone = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y,
+            transform.position.z), Quaternion.identity);
+            rbClone = bulletClone.GetComponent<Rigidbody>();
+            rbClone.AddForce(transform.forward * 1000);
+       }
+    }
+    
+
+    void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.tag == "Enemy")
+        {   
+            hp = hp - 11;
+            hpText.text = "HP: " +hp;
+            if(hp <= 0)
+            {
+            SceneManager.LoadScene(1);
+            hp = 100;
+            }
+        }
+     }
+
+      
+
+
+}
